6f317c276cda3f976e8c2ed0ae12136882dd2d04
JanardhanReddy <bommireddy246@gmail.com>
2023-07-26 13:19:24 +0100

    terraform

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ resource  "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    {name = "demo1", value = "demo1", type = "string"}
+    {name = "roboshop.dev.frontend.catalogue_url", value = "demo1", type = "string"}
+    {name = "roboshop.dev.frontend.user_url", value = "demo1", type = "string"}
+    {name = "roboshop.dev.frontend.cart_url", value = "demo1", type = "string"}
+    {name = "roboshop.dev.frontend.payment_url", value = "demo1", type = "string"}
+    {name = "roboshop.dev.frontend.shipping_url", value = "demo1", type = "string"}
   ]
 }
