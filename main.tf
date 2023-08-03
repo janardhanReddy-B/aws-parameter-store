a6df92ff0d171774c4eae6624c7112f9535d4c4e
JanardhanReddy <bommireddy246@gmail.com>
2023-08-03 21:01:56 +0100

    terraform

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource  "aws_ssm_parameter" "params" {
   name = var.params[count.index].name
   type = var.params[count.index].type
   value = var.params[count.index].value
+  overwrite = true
   key_id    = "2eeb6c9c-36f5-4e89-857c-680ad8eedf3e"
 
 }
@@ -33,6 +34,14 @@ variable "params" {
     { name = "roboshop.dev.mysql.username", value = "roboshop", type = "String" },
     { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
     { name = "roboshop.dev.docdb.username", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.user.docdb_endpoint", value = "docdb-dev.cluster-cajpnbnycbmh.us-east-1.docdb.amazonaws.com", type = "String" },
+    { name = "roboshop.dev.catalogue.docdb_endpoint", value = "docdb-dev.cluster-cajpnbnycbmh.us-east-1.docdb.amazonaws.com", type = "String" },
+    { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cajpnbnycbmh.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
+
+    { name = "roboshop.dev.user.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cajpnbnycbmh.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
+    { name = "roboshop.dev.mysql.endpoint", value = "rds-dev.cluster-cajpnbnycbmh.us-east-1.rds.amazonaws.com", type = "String" },
+    { name = "roboshop.dev.shipping.db_host", value = "rds-dev.cluster-cajpnbnycbmh.us-east-1.rds.amazonaws.com", type = "String" },
+    { name = "roboshop.dev.user.redis_host", value = "elasticache-dev-instance.dfomof.0001.use1.cache.amazonaws.com", type = "String" },
 
     # usally passwords are not allowed strore i github. need to create mannually in aws
     { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
